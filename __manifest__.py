771e7abb38282b7869a2defcd9b460170e2e4e5a
Ahmed-Daoudi <107957529+ahmedDaoudi-u@users.noreply.github.com>
2024-02-18 16:42:26 +0100

    Adding a new view and a python function

--- __manifest__.py
+++ __manifest__.py
@@ -6,11 +6,13 @@
     "description": "Real estate module for the ads of the business, anyways this is just a test",
     "data": [
         'security/ir.model.access.csv',
+        'views/Property_offer_view.xml',
         'views/property_view.xml',
         'views/Property_view_type.xml',
         'views/property_view_tag.xml',
         'views/menu_item.xml',
         'data/estate.property.type.csv'
+
     ],
     "category": "Sales",
     "depends": ['base'],
